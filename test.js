cca02553b53be5e8d67fd99b761701110934649d
Haseeb234542 <55197@students.riphah.edu.pk>
2026-04-10 09:59:19 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,10 @@
-js 
-console.log('Running tests...'); 
-// Simulate test pass 
-if (true) { 
-console.log('     
-process.exit(0); 
-} else { 
-console.error('    
-process.exit(1); 
-} 
-Tests passed'); 
-Tests failed');
+console.log('Running tests...');
+
+// Simulate test pass
+if (true) {
+  console.log('Tests passed!');
+  process.exit(0);
+} else {
+  console.error('Tests failed!');
+  process.exit(1);
+}
