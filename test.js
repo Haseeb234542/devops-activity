681ef1034d198af3e69b4af3f18036dfb1675dd5
Haseeb Ahmad <55197@students.riphah.edu.pk>
2026-04-10 09:43:43 -0700

    setup CI pipeline

---
+++ test.js
@@ -0,0 +1,12 @@
+js 
+console.log('Running tests...'); 
+// Simulate test pass 
+if (true) { 
+console.log('     
+process.exit(0); 
+} else { 
+console.error('    
+process.exit(1); 
+} 
+Tests passed'); 
+Tests failed');
